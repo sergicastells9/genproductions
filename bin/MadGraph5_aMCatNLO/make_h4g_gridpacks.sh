65e7c5d59f2e1203532d589cdc6380dbfd9484a9
Sergi Castells <scastel2@nd.edu>
2024-01-23 12:32:16 +0100

    Updates to gridpacks script

--- bin/MadGraph5_aMCatNLO/make_h4g_gridpacks.sh
+++ bin/MadGraph5_aMCatNLO/make_h4g_gridpacks.sh
@@ -2,6 +2,6 @@ for i in 15 20 25 30 35 40 45 50 55 60
 do
     ./gridpack_generation.sh "hToaaTo4gamma_ma_${i}_GeV_MLM_4f_max1j" "cards/production/13p6TeV/hToaaTo4gamma_ma_AMASS_GeV_MLM_4f_max1j/${i}_GeV"
 
-    echo "\n\n"
+    printf "\n\n"
     sleep 2
 done
